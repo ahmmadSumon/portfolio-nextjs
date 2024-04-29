444171bb526b8f8ff29fa3f43a1d2ad378b1a5ec
ahmmadSumon <arifahmmadsumon@gmail.com>
2024-04-29 12:01:36 +0600

    a

--- app/pages/HeroSection.tsx
+++ app/pages/HeroSection.tsx
@@ -57,7 +57,7 @@ const HeroSection = () => {
       <div className='absolute bottom-50 left-0 mb-16 mr-16 mt-10 hidden md:block '>
         <CircularTextWithButton/> {/* Positioned strategically */}
       </div>
-      <LampContainer className=' md:pt-[5vh]'>
+      <LampContainer className=' md:pt-[5vh] mt'>
       <motion.div
         initial={{ opacity: 0.5, y: 0 }}
         whileInView={{ opacity: 1, y: 100 }}
@@ -66,7 +66,7 @@ const HeroSection = () => {
           duration: 0.8,
           ease: "easeInOut",
         }}
-        className=" mt-20 "
+        className=" mt-52 "
       >
         <ContactForm/>
       </motion.div>
